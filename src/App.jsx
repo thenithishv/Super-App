a26cec187a830c6eb928d561f466ac05473c524b
thenithishv <thenithishv@gmail.com>
2024-12-19 14:23:08 +0530

    added not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
+import NotFoundPage from './pages/NotFoundPage'
 import RegisterPage from './pages/RegisterPage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<RegisterPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
+        <Route path='/home' element={<RegisterPage/>}/>
+        <Route path='/genres' element={<RegisterPage/>}/>
+        <Route path='/carousel' element={<RegisterPage/>}/>
+        <Route path='/dashboard' element={<RegisterPage/>}/>
+        <Route path='/movie' element={<RegisterPage/>}/>
+        <Route path='*' element={<NotFoundPage/>}/>
       </Routes>
     </BrowserRouter>
   )
