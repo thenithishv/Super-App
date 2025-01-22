c03a6b6d071bd5861abc60b1e74db8d57882e09b
Nithish K S <thenithishv@gmail.com>
2025-01-22 15:37:58 +0530

    completed Notes Widget

--- src/components/TimerWidget.jsx
+++ src/components/TimerWidget.jsx
@@ -1,6 +1,8 @@
 import {useState, useEffect} from 'react';
 import styles from './TimerWidget.module.css';
 import { FaCaretUp,FaCaretDown  } from "react-icons/fa6";
+ import { CircularProgressbar } from 'react-circular-progressbar';
+
 
 
 const hours_step = 3600;
@@ -12,6 +14,8 @@ function TimerWidget() {
 
     const [totalSeconds, setTotalSeconds] = useState(7263);
     const [isRunning, setIsRunning] = useState(false);
+    const [cachedSeconds, setCachedSeconds] = useState(10);
+
     const parseTime = (totalSeconds)=>{
         const hours = Math.floor(totalSeconds/3600);
         const minutes = Math.floor((totalSeconds%3600)/60);
@@ -37,6 +41,7 @@ function TimerWidget() {
 
     const stepHandler = (step)=>{
     if(isRunning||(step < 0 && totalSeconds -step <0)) return;
+    setCachedSeconds(totalSeconds+step);
     setTotalSeconds(totalSeconds+step);
 };
 
@@ -44,7 +49,48 @@ function TimerWidget() {
   return (
     <div className={styles.container}>
         <div className={styles.left}>
-        {totalSeconds}
+        <CircularProgressbar
+        value={totalSeconds}
+        maxValue={1}
+        text={`${(parseTime(totalSeconds).hours)}:${(parseTime(totalSeconds).minutes)}:${(parseTime(totalSeconds).seconds)}`}
+        styles={{
+    // Customize the root svg element
+    root: {},
+    // Customize the path, i.e. the "completed progress"
+    path: {
+      // Path color
+      stroke: `rgba(62, 152, 199, ${totalSeconds / 100})`,
+      // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+      strokeLinecap: 'butt',
+      // Customize transition animation
+      transition: 'stroke-dashoffset 0.5s ease 0s',
+      // Rotate the path
+      transform: 'rotate(0.25turn)',
+      transformOrigin: 'center center',
+    },
+    // Customize the circle behind the path, i.e. the "total progress"
+    trail: {
+      // Trail color
+      stroke: '#d6d6d6',
+      // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+      strokeLinecap: 'butt',
+      // Rotate the trail
+      transform: 'rotate(0.25turn)',
+      transformOrigin: 'center center',
+    },
+    // Customize the text
+    text: {
+      // Text color
+      fill: '#f88',
+      // Text size
+      fontSize: '16px',
+    },
+    // Customize background - only used when the `background` prop is true
+    background: {
+      fill: '#3e98c7',
+    },
+  }}
+        />
         
         </div>
 
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -4,6 +4,7 @@ import ProfileWidget from '../components/ProfileWidget';
 import WeatherWidget from '../components/WeatherWidget';
 import NewsWidget from '../components/NewsWidget';
 import TimerWidget from '../components/TimerWidget';
+import NotesWidget from '../components/NotesWidget';
 
 
 function DashboardPage() {
@@ -11,7 +12,7 @@ function DashboardPage() {
 
 <div className={styles.container}>
 	<div className={styles.profileWidget}><ProfileWidget/></div>
-	<div className={styles.notesWidget}>notesWidget</div>
+	<div className={styles.notesWidget}><NotesWidget/></div>
 	<div className={styles.weatherWidget}><WeatherWidget/></div>
 	<div className={styles.timerWidget}><TimerWidget/></div>
 	<div className={styles.newsWidget}><NewsWidget/></div>
