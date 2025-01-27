d1581e37a216b91a6c6c1279fcf0f7b5702acd85
Nithish K S <thenithishv@gmail.com>
2025-01-27 13:10:36 +0530

    version 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useContext } from 'react'
+import { Navigate } from 'react-router-dom'
+import { AppContext } from './context/AppContext'
 import GenrePage from './pages/GenrePage'
 import NotFoundPage from './pages/NotFoundPage'
 import RegisterPage from './pages/RegisterPage'
 import CarouselPage from './pages/CarouselPage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import DashboardPage from './pages/DashboardPage'
-import MoviePage from './pages/MoviePage'
+import MoviePage from './pages/MoviePage';
+
+
+const ProtectedRoute = ({ children}) =>{
+  const {user} = useContext(AppContext);
+  return user ? children: <Navigate to='/register'/>;
+};
 
 function App() {
   return (
@@ -12,12 +21,39 @@ function App() {
       <Routes>
         <Route path='/' element={<RegisterPage/>}/>
         <Route path='/register' element={<RegisterPage/>}/>
-        <Route path='/home' element={<RegisterPage/>}/>
-        <Route path='/genres' element={<GenrePage/>}/>
-        <Route path='/carousel' element={<CarouselPage/>}/>
-        <Route path='/dashboard' element={<DashboardPage/>}/>
-        <Route path='/movie' element={<MoviePage/>}/>
-        <Route path='*' element={<NotFoundPage/>}/>
+        <Route 
+        path='/home' 
+        element={
+          <ProtectedRoute>
+            <RegisterPage/>
+          </ProtectedRoute>
+        }/>
+        <Route path='/genres' element={
+           <ProtectedRoute>
+           <GenrePage/>
+         </ProtectedRoute>
+          }/>
+        <Route path='/carousel' element={
+           <ProtectedRoute>
+           <CarouselPage/>
+         </ProtectedRoute>
+          
+        }/>
+        <Route path='/dashboard' element={
+           <ProtectedRoute>
+           <DashboardPage/>
+         </ProtectedRoute>
+        }/>
+        <Route path='/movie' element={
+           <ProtectedRoute>
+           <MoviePage/>
+         </ProtectedRoute>
+        }/>
+        <Route path='*' element={
+           <ProtectedRoute>
+          <NotFoundPage/>
+         </ProtectedRoute>
+        }/>
       </Routes>
     </BrowserRouter>
   )
